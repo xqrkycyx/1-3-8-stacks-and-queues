aa3827c4a159cd5af5e124367a4aeb50a3899889
Will Williams <wwilliams@indeed.com>
2024-02-27 17:36:07 -0800

    Implements Queue

--- src/Queue.js
+++ src/Queue.js
@@ -13,10 +13,37 @@ class Queue {
 
   enqueue(value) {
     // your solution here
+
+    const newNode = new Node(value);
+
+    if (this.first) {
+      this.last.next = newNode;
+    } else {
+      // Set the node of the queue's next pointer to be the new node
+      this.first = newNode;
+    }
+
+    // Make the new node the last item on the queue
+    this.last = newNode;
   }
 
   dequeue() {
     // your solution here
+
+    if (this.first) {
+      const dequeued = this.first;
+
+      // Update first pointer to point to the next node of the dequeued node
+      this.first = dequeued.next;
+
+      // If the dequeued node is the last node in the queue,
+      // update the last pointer to point to `null`
+      if (dequeued === this.last) {
+        this.last = null;
+      }
+
+      return dequeued.value;
+    }
   }
 }
 
