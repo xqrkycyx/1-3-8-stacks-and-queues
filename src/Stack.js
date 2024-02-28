1f513a66cf2846edaf9b271e0a3fdee8449f955a
Will Williams <wwilliams@indeed.com>
2024-02-27 17:34:20 -0800

    Implements Stack

--- src/Stack.js
+++ src/Stack.js
@@ -12,10 +12,23 @@ class Stack {
 
   push(value) {
     // your solution here
+
+    // Create a new node,
+    // add data to the new node, and
+    // have the pointer point to the top
+    this.top = new Node(value, this.top);
+    return this;
   }
 
   pop() {
     // your solution here
+
+    /* In order to remove the top of the stack, you have to point
+          the pointer to the next item. That next item becomes the
+          top of the stack. */
+    const popped = this.top;
+    this.top = popped.next;
+    return popped.value;
   }
 }
 
